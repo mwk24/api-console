13a1b1f29b7a10b69207d4516975ab6dd5905123
mwk24 <mwkweb@gmail.com>
2010-06-11 09:42:31 -0700

    api skeleton, some cleanup

--- app.rb
+++ app.rb
@@ -6,75 +6,88 @@ class App
   
   FB_DOMAIN = 'graph.facebook.com'
   FB_BASE = 'https://' + FB_DOMAIN
+  FQL_BASE = 'http://api.facebook.com/TODO'
   APP_HOST = 'http://strong-sunrise-54.heroku.com'
   APP_ID = '146704267370'
   APP_SECRET = '7305580388d784ca83ce3661ddefeea6'
   @@access_tok = ''
   
   def self.index(env)
-    
     App::render_view('index', 
                      {'APPID' => App::APP_ID,
-                      'AUTHTOK' => @@access_tok,
-                      'ENV' => env.inspect })
+                      'AUTHTOK' => @@access_tok},
+                      env.inspect)
   end
   
   def self.auth(env)
     
     redirect_uri = env['rack.url_scheme'] + "://" + env['HTTP_HOST'] + '/auth'
     
-    if App::query_val(env, 'initiate') == '1'
-      # do the redirect
-      verify_endpoint = App::FB_BASE + "/oauth/authorize?" + 
-                        "client_id=" + App::APP_ID +
-                        "&redirect_uri=" + redirect_uri
-                      
-      [302, {"Content-Type" => "text/html", "Location" => verify_endpoint}, '']
+    # no token yet
+    if @@access_tok.empty?
       
-    else
-      # should be receiving data
       code = App::query_val(env, 'code')
       
-      # now exchange for access token
-      token_path = "/oauth/access_token?" +
-                       "client_id=" + App::APP_ID +
-                       "&redirect_uri=" + redirect_uri + 
-                       "&client_secret=" + App::APP_SECRET +
-                       "&code=" + code
+      # we have a code already, fetch access token
+      if App::query_val(env, 'code')
+        token_path = "/oauth/access_token?" +
+                         "client_id=" + App::APP_ID +
+                         "&redirect_uri=" + redirect_uri + 
+                         "&client_secret=" + App::APP_SECRET +
+                         "&code=" + code
       
-      http = Net::HTTP.new(FB_DOMAIN, 443)
-      http.use_ssl = true
+        http = Net::HTTP.new(FB_DOMAIN, 443)
+        http.use_ssl = true
       
-      res, data = http.get(token_path)
+        res, data = http.get(token_path)
       
-      unless data.match('error')
-        # store token
-        @@access_tok = App::extract_val(data, 'access_token')
+        unless data.match('error')
+          # store token
+          @@access_tok = App::extract_val(data, 'access_token')
+        end
+      
+      # no code yet - need to redirect and get one
+      else
+        verify_endpoint = App::FB_BASE + "/oauth/authorize?" + 
+                          "client_id=" + App::APP_ID +
+                          "&redirect_uri=" + redirect_uri
+
+        return [302, {"Content-Type" => "text/html", "Location" => verify_endpoint}, '']
+      
       end
       
+      # if we got here we should have a token
       App::render_view('api', {"TOK" => @@access_tok }, data)
     end
   end
   
   def self.api(env)
-    [200, {"Content-Type" => "text/html"}, "call"]
-  end
-  
-  def self.db_val(key)
     
-    f = File.new('db.txt','r')
+    api_call = App::query_val(env, 'api')
+    api_type = App::query_val(env, 'api_type')
     
-    entries = f.readlines
+    if api_call and api_type
     
-    entries.each do |e|
-      split = e.split(':')
-      if split[0] == key
-        return split[1].strip
+      if api_type == 'fql'
+        base = App::FQL_BASE
+        path = '/' + api_call + @@access_tok
+      else
+        base = App::FB_BASE
+        path = '/' + api_call + @@access_tok
       end
-    end
-    return ''
-  end
+    
+      http = Net::HTTP.new(base, 443)
+      http.use_ssl = true
   
+      res, data = http.get(path)
+    
+      App::render_view('api', {'TOK' => @@access_tok, 'REQ' => base + path, 'RET' => data})
+    
+    else
+      App::render_view('api', {'TOK' => @@access_tok})
+    end
+    
+  end
   
   def self.extract_val(serialized, key)
     
@@ -82,20 +95,18 @@ class App
     params.each do |p|
       split = p.split('=')
       if split[0] == key
-        return split[1].strip
+        return split[1].strip rescue nil
       end
     end
     return nil
   end
   
   def self.query_val(env, key)
-  
     query = env['QUERY_STRING']
     return App::extract_val(query, key)
-    
   end
   
-  def self.render_view(name, values={}, dump='')
+  def self.render_view(name, values={}, dump='', cookie='')
     
     view_dir = "views"
     
@@ -118,10 +129,26 @@ class App
       markup.gsub!("!!#{k}!!", v)
     end
     
-    # dump some stuff
-    markup += dump
+    # dump area for general inspection
+    markup += ('<br/><br/><br/>--------------<br/>' + dump)
     
-    return [200, {"Content-Type" => "text/html"}, markup]
+    return [200, {"Content-Type" => "text/html", "Set-Cookie" => "#{cookie}\naccess_tok=#{@@access_tok}"}, markup]
   
   end
+  
+  def self.db_val(key)
+    
+    f = File.new('db.txt','r')
+    
+    entries = f.readlines
+    
+    entries.each do |e|
+      split = e.split(':')
+      if split[0] == key
+        return split[1].strip
+      end
+    end
+    return ''
+  end
+  
 end
